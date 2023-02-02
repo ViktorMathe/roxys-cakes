75f3d5351425cf97b03ca7621b8102f221d74c23
ViktorMathe <matheviktor@icloud.com>
2023-02-02 13:21:56 +0000

    Added Allowed hosts

--- roxys_cakes/settings.py
+++ roxys_cakes/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roxys-cakes-vm.herokuapp.com', 'localhost']
 
 
 # Application definition
