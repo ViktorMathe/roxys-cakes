5d709db074944b1009d991ad32f6e56a58d1866f
ViktorMathe <matheviktor@icloud.com>
2023-02-09 10:55:03 +0000

    Added view for contact us messages

--- contactus/views.py
+++ contactus/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import Contact_usForm
+from django.contrib.auth.decorators import login_required
+from .models import Contact_us
 
 
 def contact_us(request):
@@ -11,3 +13,11 @@ def contact_us(request):
         form = Contact_usForm()
     context = {'form': form}
     return render(request, 'contact_us.html', context)
+
+
+@login_required
+def messages(request):
+    messages = Contact_us.objects.filter()
+    context = {'messages': messages}
+    template = 'messages.html'
+    return render(request, template, context)
