b146bd528f8ed93d484363754a953657d8de9f32
ViktorMathe <matheviktor@icloud.com>
2023-02-11 19:00:41 +0000

    Changed AWS name

--- roxys_cakes/settings.py
+++ roxys_cakes/settings.py
@@ -199,10 +199,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'roxys-cakes-vm'
+    AWS_STORAGE_BUCKET_NAME = 'roxys-cakes-b-vm'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
