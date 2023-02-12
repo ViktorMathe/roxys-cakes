24bf24c715fd80a5ee583671bd5e4869179ab37f
ViktorMathe <matheviktor@icloud.com>
2023-02-12 21:57:48 +0000

    Added Newsletter subcriber option

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscribe(models.Model):
+    email = models.EmailField(unique=True)
+    conf_number = models.CharField(max_length=12, null=False, editable=False)
+    confirmed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email + "(" + (
+            "not" if not self.confirmed else "") + "confirmed)"
--- home/views.py
+++ home/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Subscribe
+from .forms import SubscribeForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
+
+
+def add_subscriber(request):
+    form = SubscribeForm()
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            email = post_data.get('email',)
+            form.save()
+            email = email
+            subject = render_to_string(
+                'newsletter_emails/newsletter_confirmation_subject.txt')
+            body = render_to_string(
+                'newsletter_emails/newsletter_confirmation_body.txt')
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [email])
+    template = 'newsletter.html'
+    context = {'form': form}
+    return render(request, template, context)
