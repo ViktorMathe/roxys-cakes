02011c7383c594019b954028e8f28e6c11f8727b
ViktorMathe <matheviktor@icloud.com>
2023-02-20 10:37:28 +0000

    Fixed toast messages closing button

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-$('.toast').show()
+let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+let toastList = toastElList.map(function (toastEl) {
+    let option = {
+        animation: true,
+        autohide: true,
+        delay: 5000,
+    }
+  let bsToast = new bootstrap.Toast(toastEl, option)
+  bsToast.show();
+})
