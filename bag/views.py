97704ec01fb8849185866a53863804aeb9bd42d6
ViktorMathe <matheviktor@icloud.com>
2023-02-21 13:30:41 +0000

    Added try functions to the bag app

--- bag/views.py
+++ bag/views.py
@@ -10,18 +10,24 @@ def bag_view(request):
 
 def add_bag_content(request, cake_id):
     cake = get_object_or_404(Cake, pk=cake_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
 
-    if cake_id in list(bag.keys()):
-        bag[cake_id] += quantity
-        messages.success(
-            request, f'Your bag has been updated with {cake.name}')
+    try:
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+        if cake_id in list(bag.keys()):
+            bag[cake_id] += quantity
+            messages.success(
+                request, f'Your bag has been updated with {cake.name}')
 
-    else:
-        bag[cake_id] = quantity
-        messages.success(request, f'Added {cake.name} to your bag')
+        else:
+            bag[cake_id] = quantity
+            messages.success(request, f'Added {cake.name} to your bag')
+    except Exception:
+        messages.error(
+            request, 'Have to provide a number in the quantity \
+                field to add the cake to the bag!')
+        return redirect('cake_details', cake.id)
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -29,18 +35,25 @@ def add_bag_content(request, cake_id):
 
 def change_bag(request, cake_id):
     cake = get_object_or_404(Cake, pk=cake_id)
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
 
-    if quantity > 0:
-        bag[cake_id] = quantity
-        messages.success(
-            request,
-            f'Quantity for {cake.name} has been changed to {quantity}')
+    try:
+        quantity = int(request.POST.get('quantity'))
+        bag = request.session.get('bag', {})
+        if quantity > 0:
+            bag[cake_id] = quantity
+            messages.success(
+                request,
+                f'Quantity for {cake.name} has been changed to {quantity}')
 
-    else:
+        else:
+            bag.pop(cake_id)
+            messages.warning(
+                request, f'{cake.name} has been deleted from your bag')
+    except Exception:
+        bag = request.session.get('bag', {})
         bag.pop(cake_id)
-        messages.warning(request, f'{cake.name} has been deleted from your bag')
+        messages.error(request, f'{cake.name} has been removed from your bag!')
+        return redirect(reverse('bag_view'))
 
     request.session['bag'] = bag
     return redirect(reverse('bag_view'))
@@ -52,10 +65,11 @@ def delete_from_bag(request, cake_id):
         bag = request.session.get('bag', {})
 
         bag.pop(cake_id)
-        messages.warning(request, f'{cake.name} has been removed from your bag')
+        messages.warning(
+            request, f'{cake.name} has been removed from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-    except Exception as e:
+    except Exception:
         messages.error(request, 'You have no cake like this in your bag!')
         return HttpResponse(status=500)
