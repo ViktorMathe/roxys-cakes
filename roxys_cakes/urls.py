8e250f4fe765ace12c226c9d75f475dbc4fe86d6
ViktorMathe <matheviktor@icloud.com>
2023-02-21 20:08:51 +0000

    Added custom 404 page

--- roxys_cakes/urls.py
+++ roxys_cakes/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,5 @@ urlpatterns = [
     path('reviews/', include('reviews.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'roxys_cakes.views.handler404'
---
+++ roxys_cakes/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """ Custom 404 page """
+    return render(request, "errors/404.html", status=404)
